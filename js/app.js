f28f5737820b0c297a11423b13e16b801f3f65ec
dpaola5235 <78803623+dpaola5235@users.noreply.github.com>
2021-11-13 21:02:57 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -7,9 +7,12 @@ let btnCameraBack = $('#btnCameraBack');
 let btnTakePhoto = $('#btnTakePhoto');
 let photoUser = $('#photoUser');
 
+let photoText = "";
+
 const camera = new Camera(player[0]);
 
 btnCamera.on('click',()=>{
+    photoText = "Camara Frontal"
     camera.on().then(result=>{
         if(!result){
             alert("Error al iniciar la camara")
@@ -18,6 +21,7 @@ btnCamera.on('click',()=>{
 });
 
 btnCameraBack.on('click',()=>{
+    photoText = "Camara Trasera"
     camera.onBack().then(result=>{
         if(!result){
             alert("Error al iniciar la camara")
@@ -27,10 +31,24 @@ btnCameraBack.on('click',()=>{
 
 btnTakePhoto.on('click',()=>{
     camera.off();
-    photoUser.attr('src',camera.takePhoto());
+    //photoUser.attr('src',);
+    let photo = camera.takePhoto();
+    let image = createImage(photo,photoText);
+    $('#cardPhoto').append(image)
 });
 
+// <img id="photoUser" src="img/user.png" width="300px" height="300px">
 
+function createImage(img, photo_text) {
+
+    let card = $(`
+    <div class="col-12 text-center">
+        <h1 class="text-center">${photo_text}</h1>
+        <img style="width: 300px; height: 300px;" src="${img}">        
+    </div>
+    `)
+    return card
+}
 
 if(navigator.serviceWorker){
     if(url.includes('localhost')){
